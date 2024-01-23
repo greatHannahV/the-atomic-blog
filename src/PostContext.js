38cf234f799b0f3634f64ea83252e7c8c0e33294
greatHannahV <i.kasl@mail.ru>
2024-01-23 17:58:45 +0200

    memo $ useMemo

---
+++ src/PostContext.js
@@ -0,0 +1,70 @@
+import { createContext, useState, useEffect, useContext } from 'react'
+import { faker } from '@faker-js/faker'
+
+function createRandomPost() {
+  return {
+    title: `${faker.hacker.adjective()} ${faker.hacker.noun()}`,
+    body: faker.hacker.phrase(),
+  }
+}
+
+//1 ctreat a new context
+const PostContext = createContext()
+
+function PostProvider({ children }) {
+  const [isFakeDark, setIsFakeDark] = useState(false)
+
+  // Whenever `isFakeDark` changes, we toggle the `fake-dark-mode` class on the HTML element (see in "Elements" dev tool).
+  useEffect(
+    function () {
+      document.documentElement.classList.toggle('fake-dark-mode')
+    },
+    [isFakeDark],
+  )
+
+  const [posts, setPosts] = useState(() =>
+    Array.from({ length: 30 }, () => createRandomPost()),
+  )
+  const [searchQuery, setSearchQuery] = useState('')
+
+  // Derived state. These are the posts that will actually be displayed
+  const searchedPosts =
+    searchQuery.length > 0
+      ? posts.filter((post) =>
+          `${post.title} ${post.body}`
+            .toLowerCase()
+            .includes(searchQuery.toLowerCase()),
+        )
+      : posts
+
+  function handleAddPost(post) {
+    setPosts((posts) => [post, ...posts])
+  }
+
+  function handleClearPosts() {
+    setPosts([])
+  }
+
+  return (
+    //2 provide value to child comp
+    <PostContext.Provider
+      value={{
+        posts: searchedPosts,
+        onClearPosts: handleClearPosts,
+        onAddPost: handleAddPost,
+        searchQuery,
+        setSearchQuery,
+        isFakeDark,
+        setIsFakeDark,
+      }}
+    >
+      {children}
+    </PostContext.Provider>
+  )
+}
+function usePosts() {
+  const context = useContext(PostContext)
+  if (context === undefined) throw new Error('Outside of the provider')
+  return context
+}
+export { PostProvider, usePosts }
